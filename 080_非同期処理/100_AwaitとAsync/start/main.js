aff004fb065ee3a82e713bee48f28612f64eece3
39takagikouta <kouta.takagi0728@gmail.com>
2024-03-20 18:04:26 +0900

    非同期処理のメモ修了

--- 080_非同期処理/100_AwaitとAsync/start/main.js
+++ 080_非同期処理/100_AwaitとAsync/start/main.js
@@ -1,20 +1,25 @@
 function sleep(val) {
-  return new Promise(function(resolve) {
-    setTimeout(function() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
       console.log(val++);
       resolve(val);
     }, 1000);
   });
 }
 
-sleep(0).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-}).then(function(val) {
-  return sleep(val);
-})
+sleep(0)
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  })
+  .then(function (val) {
+    return sleep(val);
+  });
